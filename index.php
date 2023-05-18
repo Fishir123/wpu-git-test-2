92fb77dccdfc9c3e5efe1ea8740746145295113d
Fishir123 <faiqthestrongest@gmail.com>
2023-05-18 19:12:32 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello Word!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, blanditiis! Fugit necessitatibus amet illo facere libero quaerat dolore, assumenda corrupti suscipit dolorum, odio repellendus vero possimus ad ut magnam. Ut.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
